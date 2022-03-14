d54de77c9d691db63d8a3d06f8d5b16a995a12b4
jotaalvim <joaoafonsoalvim@gmail.com>
2022-03-14 23:51:30 +0000

    version 1.1

--- densidade.py
+++ densidade.py
@@ -3,14 +3,15 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 from matplotlib.colors import LogNorm, Normalize
 from mpl_toolkits.axes_grid1 import AxesGrid
-
 import re
 #import sys
+
+#lances de cavalo
+#grep -Po '\bNx?\K[a-h][1-8]' /home/jotaalvim/Documents/bases_dados/outras/lichess_jotaalvim_2021-03-23.pgn | sort | uniq -c  | sort -n
 
 #jogos = sys.argv[1]
 jogos = "/home/jotaalvim/Documents/bases_dados/outras/lichess_jotaalvim_2022-03-12.pgn"
 #jogos = "/home/jotaalvim/Downloads/lichess_DiogoCipreste_2022-03-12.pgn"
-
 #jogos = "/home/jotaalvim/Downloads/lichess_Lucena0202_2022-03-11.pgn"
 #jogos = "/home/jotaalvim/Downloads/lichess_Portomas_2022-03-11.pgn"
 #jogos = "/home/jotaalvim/Downloads/lichess_LordVeldergrath_2022-03-13.pgn"
@@ -23,15 +24,14 @@ texto = f.read()
 username = "jotaalvim"
 
 #Logarithm scale option
-#log = False
-log = True
+log = False
+#log = True
 
 #color of analysis  'black', 'white', 'both'
 color = 'white'
 
 
 def getGames(color):
-    #games = re.findall(r'\n1.*',texto)
     if (color == 'white'):
         games = re.findall(fr'White "{username}"(?:.|\n)*?\n(1\..*)',texto)
     if (color == 'black'):
@@ -40,184 +40,135 @@ def getGames(color):
 
 games = getGames(color)
 
-dKnight = {}
-dBishop = {}
-dQueen  = {}
-dRook   = {}
-dPawn   = {}
-dKing   = {}
-
-#lances de cavalo
-#grep -Po '\bNx?\K[a-h][1-8]' /home/jotaalvim/Documents/bases_dados/outras/lichess_jotaalvim_2021-03-23.pgn | sort | uniq -c  | sort -n
+dic = { 'N' : {}, 'B' : {}, 'Q' : {}, 'K' : {}, 'R' : {}, 'P' : {} } 
 
 for jogo in games:
-    #for pos in re.findall(r'[0-9]+\. Nx?([a-z][1-8])',jogo):
-    for pos in re.findall(r'\b[1-6]\. Nx?([a-z][1-8])',jogo):
-        if pos in dKnight:
-            dKnight[pos] += 1
+    i = 1
+    fim = 20
+
+    gameSlice = re.findall(fr'\b{i}\. (.*?)(?:{fim+1}|1000)',jogo+'1000')[0]
+
+    if (color == 'black'):
+        pretas = r'\S+ '
+    else:
+        pretas = ''
+
+    for peca,pos in re.findall(fr'\b[0-9]+\. {pretas}([NQBRK])x?([a-z][1-8])', gameSlice):
+        if pos in dic[peca]:
+            dic[peca][pos] += 1
         else:
-            dKnight[pos] = 1
+            dic[peca][pos]  = 1
 
-    #for pos in re.findall(r'[0-9]+\. Bx?([a-z][1-8])',jogo):
-    for pos in re.findall(r'\b[1-6]\. Bx?([a-z][1-8])',jogo):
-        if pos in dBishop:
-            dBishop[pos] += 1
+    ##for pos in re.findall(r'[0-9]+\. Nx?([a-z][1-8])',jogo):
+    #for pos in re.findall(r'\b[1-6]\. Nx?([a-z][1-8])',jogo):
+    #    if pos in dKnight:
+    #        dKnight[pos] += 1
+    #    else:
+    #        dKnight[pos] = 1
+
+    ##for pos in re.findall(r'[0-9]+\. Bx?([a-z][1-8])',jogo):
+    #for pos in re.findall(r'\b[1-6]\. Bx?([a-z][1-8])',jogo):
+    #    if pos in dBishop:
+    #        dBishop[pos] += 1
+    #    else:
+    #        dBishop[pos] = 1
+
+    ##for pos in re.findall(r'[0-9]+\. Qx?([a-z][1-8])',jogo):
+    #for pos in re.findall(r'\b[1-6]\. Qx?([a-z][1-8])',jogo):
+    #    if pos in dQueen:
+    #        dQueen[pos] += 1
+    #    else:
+    #        dQueen[pos] = 1
+
+    ##for pos in re.findall(r'[0-9]+\. Kx?([a-z][1-8])',jogo):
+    #for pos in re.findall(r'\b[1-9]\. Kx?([a-z][1-8])',jogo):
+    #    if pos in dKing:
+    #        dKing[pos] += 1
+    #    else:
+    #        dKing[pos] = 1
+
+    ##for pos in re.findall(r'[0-9]+\. Rx?([a-z][1-8])',jogo):
+    #for pos in re.findall(r'\b[1-6]\. Rx?([a-z][1-8])',jogo):
+    #    if pos in dRook:
+    #        dRook[pos] += 1
+    #    else:
+    #        dRook[pos] = 1
+
+    for pos in re.findall(fr'[0-9]+\. {pretas}(?:[a-h]x)?([a-z][1-8])',gameSlice):
+        if pos in dic['P']:
+            dic['P'][pos] += 1
         else:
-            dBishop[pos] = 1
-
-    #for pos in re.findall(r'[0-9]+\. Qx?([a-z][1-8])',jogo):
-    for pos in re.findall(r'\b[1-6]\. Qx?([a-z][1-8])',jogo):
-        if pos in dQueen:
-            dQueen[pos] += 1
-        else:
-            dQueen[pos] = 1
-
-    #for pos in re.findall(r'[0-9]+\. Rx?([a-z][1-8])',jogo):
-    for pos in re.findall(r'\b[1-6]\. Rx?([a-z][1-8])',jogo):
-        if pos in dRook:
-            dRook[pos] += 1
-        else:
-            dRook[pos] = 1
-
-    #for pos in re.findall(r'[0-9]+\. (?:[a-h]x)?([a-z][1-8])',jogo):
-    for pos in re.findall(r'\b[0-6]\. (?:[a-h]x)?([a-z][1-8])',jogo):
-        i = 1
-        fim = 10
-
-        #bocado = re.findall(f'{i}\. .* {fim+1}',jogo)
-        
-        #PORQUE NAO FUNCIONA ↓
-        #bocado = re.findall(f'\b{i}\. .* {fim+1}',jogo)
-
-        #                                 fim = 300
-        gameSlice = re.findall(f'{i}\. .* (?:{fim})?',jogo)
-
-
-        if pos in dPawn:
-            dPawn[pos] += 1
-        else:
-            dPawn[pos] = 1
-
-    #for pos in re.findall(r'[0-9]+\. Kx?([a-z][1-8])',jogo):
-    for pos in re.findall(r'\b[1-9]\. Kx?([a-z][1-8])',jogo):
-        if pos in dKing:
-            dKing[pos] += 1
-        else:
-            dKing[pos] = 1
+            dic['P'][pos]  = 1
 
 
 # quando não ha valores poe-nos a 0
 for c in ['a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'e1', 'e2', 'e3', 'e4', 'e5', 'e6', 'e7', 'e8', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8']:
-    if c not in dKnight:
-        dKnight[c] = 0
-    if c not in dBishop:
-        dBishop[c] = 0
-    if c not in dQueen:
-        dQueen[c] = 0
-    if c not in dRook:
-        dRook[c] = 0
-    if c not in dPawn:
-        dPawn[c] = 0
-    if c not in dKing:
-        dKing[c] = 0
+    for key in 'NQBPKR':
+        if c not in dic[key]:
+            dic[key][c] = 0
 
 
 # cria uma lista com os valores
-lN = []
-lB = []
-lQ = []
-lR = []
-lP = []
-lK = []
+l = { 'N' : [], 'B' : [], 'Q' : [], 'R' : [], 'P' : [], 'K' : [] }
 
-for val in sorted(dKnight):
-    lN.append(dKnight[val])
-for val in sorted(dBishop):
-    lB.append(dBishop[val])
-for val in sorted(dQueen):
-    lQ.append(dQueen[val])
-for val in sorted(dRook):
-    lR.append(dRook[val])
-for val in sorted(dPawn):
-    lP.append(dPawn[val])
-for val in sorted(dKing):
-    lK.append(dKing[val])
+for key in 'NBQRPK':
+    for val in sorted(dic[key]):
+        l[key].append(dic[key][val])
+
+#for val in sorted(dKing):
+#    lK.append(dKing[val])
 
 
 # parte uma lista numa matriz de listas de tamanho 8
 n = 8
+
+dPlot = { 'N':None, 'Q':None, 'P':None, 'K':None, 'R':None, 'B':None}
 #    "plot Night"
-pN = [lN[i:i + n] for i in range(0, len(lN), n)] 
-pB = [lB[i:i + n] for i in range(0, len(lB), n)] 
-pQ = [lQ[i:i + n] for i in range(0, len(lQ), n)] 
-pR = [lR[i:i + n] for i in range(0, len(lR), n)] 
-pP = [lP[i:i + n] for i in range(0, len(lP), n)] 
-pK = [lK[i:i + n] for i in range(0, len(lK), n)] 
+for key in 'NQPKRB':
+    dPlot[key] = [l[key][i:i + n] for i in range(0, len(l[key]), n)] 
+
+#pK = [lK[i:i + n] for i in range(0, len(lK), n)] 
 
 # trapalhada para por o tabuleiro com orientação das brancas
-for row in pN:
-    row.reverse()
-for row in pB:
-    row.reverse()
-for row in pQ:
-    row.reverse()
-for row in pR:
-    row.reverse()
-for row in pP:
-    row.reverse()
-for row in pK:
-    row.reverse()
 
-numpy_array = np.array(pN)
-transpose = numpy_array.T
-pN = transpose.tolist()
+for key in 'NQPKRB':
+    for row in dPlot[key]:
+        row.reverse()
+#for row in pK:
+#    row.reverse()
 
-numpy_array = np.array(pB)
-transpose = numpy_array.T
-pB = transpose.tolist()
+for key in 'NQPKRB':
+    numpy_array = np.array(dPlot[key])
+    transpose = numpy_array.T
+    dPlot[key] = transpose.tolist()
 
-numpy_array = np.array(pQ)
-transpose = numpy_array.T
-pQ = transpose.tolist()
+#numpy_array = np.array(pK)
+#transpose = numpy_array.T
+#pK = transpose.tolist()
 
-numpy_array = np.array(pR)
-transpose = numpy_array.T
-pR = transpose.tolist()
-
-numpy_array = np.array(pP)
-transpose = numpy_array.T
-pP = transpose.tolist()
-
-numpy_array = np.array(pK)
-transpose = numpy_array.T
-pK = transpose.tolist()
-
-#plt.imshow( pN , cmap = 'binary')
-#plt.imshow( pN , cmap = 'binary' , interpolation = 'nearest',  norm=LogNorm())
-##plt.imshow( pN ,  norm=LogNorm())
 #plt.title( "Knight heat map" )
 
-
 fig, axes = plt.subplots(ncols=6, figsize=(80, 15))
-#fig, axes = plt.subplots(ncols=6)
 #fig.set_figheight(80)
 #fig.set_figwidth(15)
+
 ax1, ax2, ax3, ax4, ax5, ax6 = axes
 
 if (log):
-    im1 = ax1.matshow(pN, cmap = 'binary', norm=LogNorm())
-    im2 = ax2.matshow(pB, cmap = 'binary', norm=LogNorm())
-    im3 = ax3.matshow(pQ, cmap = 'binary', norm=LogNorm())
-    im4 = ax4.matshow(pR, cmap = 'binary', norm=LogNorm())
-    im5 = ax5.matshow(pP, cmap = 'binary', norm=LogNorm())
-    im6 = ax6.matshow(pK, cmap = 'binary', norm=LogNorm())
-elif:
-    im1 = ax1.matshow(pN, cmap = 'binary')
-    im2 = ax2.matshow(pB, cmap = 'binary')
-    im3 = ax3.matshow(pQ, cmap = 'binary')
-    im4 = ax4.matshow(pR, cmap = 'binary')
-    im5 = ax5.matshow(pP, cmap = 'binary')
-    im6 = ax6.matshow(pK, cmap = 'binary')
+    im1 = ax1.matshow(dPlot['N'], cmap = 'binary', norm=LogNorm())
+    im2 = ax2.matshow(dPlot['Q'], cmap = 'binary', norm=LogNorm())
+    im3 = ax3.matshow(dPlot['B'], cmap = 'binary', norm=LogNorm())
+    im4 = ax4.matshow(dPlot['P'], cmap = 'binary', norm=LogNorm())
+    im5 = ax5.matshow(dPlot['K'], cmap = 'binary', norm=LogNorm())
+    im6 = ax6.matshow(dPlot['R'], cmap = 'binary', norm=LogNorm())
+else:
+
+    im1 = ax1.matshow(dPlot['N'])
+    im2 = ax2.matshow(dPlot['Q'])
+    im3 = ax3.matshow(dPlot['B'])
+    im4 = ax4.matshow(dPlot['P'])
+    im5 = ax5.matshow(dPlot['K'])
+    im6 = ax6.matshow(dPlot['R'])
 
 #plt.title( "Knight heat map" )
 plt.savefig('assets/teste.svg', dpi= 100)
