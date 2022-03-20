31eb9e0a7b15105eaa5beba6fb85c2e0bfb5643d
jotaalvim <joaoafonsoalvim@gmail.com>
2022-03-20 21:44:25 +0000

    bug fix

--- README.md
+++ README.md
@@ -6,18 +6,20 @@ The darker the color means that a peace have been moved there more times.
 This are the heatmaps from the knigth, bishop, queen, rooks , pawns and the king, respectively, in the player's white games.
 
 Heatmaps from a weaker player (800 blitz lichess)
-![Screen shot](./assets/1.2.png)
+![Screen shot](./assets/1.png)
 
 Heatmaps from my ~3000 games on lichess (2000 blitz lichess)
-![Screen shot](./assets/2.2.png)
+![Screen shot](./assets/2.png)
 
 Heatmaps from a very strong player (2700 blitz lichess)
-![Screen shot](./assets/3.2.png)
+![Screen shot](./assets/3.png)
 
 ## It's cool but not that informative
 For now there's not much information that we can collect, we can perhaps
-conclude that the weaker players doen't care so much for king safety and place them it in the center very often.
+c onclude that the weaker players ofte play the queen, for example, to the more obvious d1-h5
+diagonal instead of placing in c2 or d2.
 
+## Diferences between players 
 ## For the future
 A more useful/interesting data would be the logarithm diferencies between each
 player suares.
--- densidade.py
+++ densidade.py
@@ -5,18 +5,19 @@ from matplotlib.colors import LogNorm, Normalize
 from mpl_toolkits.axes_grid1 import AxesGrid
 import re
 from jjcli import * 
+import math
 #import sys
 
 #grep -Po '\bNx?\K[a-h][1-8]' /home/jotaalvim/Documents/bases_dados/outras/lichess_jotaalvim_2021-03-23.pgn | sort | uniq -c  | sort -n
 
 #jogos = sys.argv[1]
 
-jogos = "/home/jotaalvim/Documents/bases_dados/outras/lichess_jotaalvim_2022-03-12.pgn"
-#jogos = "/home/jotaalvim/Downloads/lichess_DiogoCipreste_2022-03-12.pgn"
+jogos2 = "/home/jotaalvim/Documents/bases_dados/outras/lichess_jotaalvim_2022-03-12.pgn"
+jogos = "/home/jotaalvim/Downloads/lichess_DiogoCipreste_2022-03-12.pgn"
 #jogos = "/home/jotaalvim/Downloads/lichess_Lucena0202_2022-03-11.pgn"
 #jogos = "/home/jotaalvim/Downloads/lichess_Portomas_2022-03-11.pgn"
 #jogos = "/home/jotaalvim/Downloads/lichess_LordVeldergrath_2022-03-13.pgn" 
-jogos2 = "/home/jotaalvim/Downloads/lichess_Tigran-Harutyunyan_2022-03-15.pgn" 
+jogos3 = "/home/jotaalvim/Downloads/lichess_Tigran-Harutyunyan_2022-03-15.pgn" 
 
 
 #c = clfilter(opt="do:")     ## option values in c.opt dictionary
@@ -27,13 +28,13 @@ f = open(jogos, "r")
 texto = f.read() 
 
 #username of the person in study, might be automated later?
-username = "jotaalvim"
+username = "DiogoCipreste"
+username2= "jotaalvim"
 #username = "Portomas"
 #username = "LordVeldergrath"
-username2 = "Tigran-Harutyunyan"
+username3 = "Tigran-Harutyunyan"
 
 c = clfilter(opt="blu:e:")
-
 
 #Number of moves option
 if '-e' in c.opt:
@@ -67,6 +68,9 @@ def getGames(color,path, username):
 def moveTable (color, path, username):
     games = getGames(color, path, username)
 
+    
+
+
     dic = { 'N':{}, 'B':{}, 'Q':{}, 'K':{}, 'R':{}, 'P':{} } 
 
     for jogo in games:
@@ -96,40 +100,85 @@ def moveTable (color, path, username):
                 dic['P'][pos]  = 1
 
 
-# quando não ha valores poe-nos a 0
+        # TROCAR O-O por Rg1 e Tf1 (se calhar a torre não)
+        # n vai ser sempre 1 no máximo
+
+        #white short castle
+        if color == 'white':
+            wsc= re.findall(r'\d+\. O-O',jogo)
+            n = len(wsc)
+            if 'g1' in dic['K']:
+                dic['K']['g1'] += n
+            else:
+                dic['K']['g1'] = n
+            if 'f1' in dic['R']:
+                dic['R']['f1'] += n
+            else:
+                dic['R']['f1'] = n
+            #white long castle
+            wlc= re.findall(r'\d+\. O-O-O',jogo)
+            n = len(wlc)
+            if 'c1' in dic['K']:
+                dic['K']['c1'] += n
+            else:
+                dic['K']['c1'] = n
+            if 'd1' in dic['R']:
+                dic['R']['d1'] += n
+            else:
+                dic['R']['d1'] = n
+
+        if color == 'black':
+            #black short castle
+            bsc= re.findall(r'\d+\. \w+ O-O',jogo)
+            n = len(bsc)
+            if 'g8' in dic['K']:
+                dic['K']['g8'] += n
+            else:
+                dic['K']['g8'] = n
+
+            if 'f8' in dic['R']:
+                dic['R']['f8'] += n
+            else:
+                dic['R']['f8'] = n
+            #black long castle
+            blc= re.findall(r'\d+\. \w+ O-O-O',jogo)
+            n = len(blc)
+            if 'c8' in dic['K']:
+                dic['K']['c8'] += n
+            else:
+                dic['K']['c8'] = n
+            if 'd8' in dic['R']:
+                dic['R']['d8'] += n
+            else:
+                dic['R']['d8'] = n
+
+    # quando não ha valores poe-nos a 0
     for c in ['a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'e1', 'e2', 'e3', 'e4', 'e5', 'e6', 'e7', 'e8', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8']:
         for key in 'NQBPKR':
             if c not in dic[key]:
                 dic[key][c] = 0
 
-# cria uma lista com os valores
+    # cria uma lista com os valores
     l = { 'N' : [], 'B' : [], 'Q' : [], 'R' : [], 'P' : [], 'K' : [] }
 
     for key in 'NBQRPK':
         for val in sorted(dic[key]):
             l[key].append(dic[key][val])
 
-#for val in sorted(dKing):
-#    lK.append(dKing[val])
 
 
-# parte uma lista numa matriz de listas de tamanho 8
+    # parte uma lista numa matriz de listas de tamanho 8
     n = 8
 
     dPlot = { 'N':None, 'Q':None, 'P':None, 'K':None, 'R':None, 'B':None}
-#    "plot Night"
     for key in 'NQPKRB':
         dPlot[key] = [l[key][i:i + n] for i in range(0, len(l[key]), n)] 
 
-#pK = [lK[i:i + n] for i in range(0, len(lK), n)] 
-
-# trapalhada para por o tabuleiro com orientação das brancas
+    # trapalhada para por o tabuleiro com orientação das brancas
 
     for key in 'NQPKRB':
         for row in dPlot[key]:
             row.reverse()
-#for row in pK:
-#    row.reverse()
 
     for key in 'NQPKRB':
         numpy_array = np.array(dPlot[key])
@@ -138,13 +187,38 @@ def moveTable (color, path, username):
 
     return dPlot
 
-#numpy_array = np.array(pK)
-#transpose = numpy_array.T
-#pK = transpose.tolist()
+
 
 #plt.title( "Knight heat map" )
+
+def countGames (color, path,username):
+    return len(getGames(color,path, username))
+
+def difTable (color, jogos, jogos2 ,username, username2):
+    dPlot  = moveTable(color, jogos , username) 
+    dPlot2 = moveTable(color, jogos2, username2) 
+    
+    ngames  = countGames(color, jogos, username)
+    ngames2 = countGames(color, jogos2, username2)
+    
+    #delta = { 'N':[], 'Q':[[]], 'P':[[]], 'K':[[]], 'R':[[]], 'B':[[]]}
+    delta = dPlot.copy()
+
+    for key in 'NBRKPQ':
+        for i in range(8):
+            for j in range(8):
+                # add 1 because log(-1) == -infininty
+                delta[key][i][j] = math.log((dPlot2[key][i][j])/ngames2 +1) - math.log((dPlot[key][i][j])/ngames +1)
+
+    return delta
+
+
+
+#dPlot = difTable (color, jogos, jogos2, username , username2)
 dPlot  = moveTable(color, jogos , username) 
-dPlot2 = moveTable(color, jogos2, username2) 
+#dPlot = moveTable(color, jogos2, username2) 
+
+
 
 fig, axes = plt.subplots(ncols=6, figsize=(80, 15))
 #fig.set_figheight(80)
@@ -153,12 +227,13 @@ fig, axes = plt.subplots(ncols=6, figsize=(80, 15))
 ax1, ax2, ax3, ax4, ax5, ax6 = axes
 
 if (log):
-    im1 = ax1.matshow(dPlot['P'], cmap = 'binary', norm=LogNorm())
-    im2 = ax2.matshow(dPlot['N'], cmap = 'binary', norm=LogNorm())
-    im3 = ax3.matshow(dPlot['B'], cmap = 'binary', norm=LogNorm())
-    im4 = ax4.matshow(dPlot['R'], cmap = 'binary', norm=LogNorm())
-    im5 = ax5.matshow(dPlot['Q'], cmap = 'binary', norm=LogNorm())
-    im6 = ax6.matshow(dPlot['K'], cmap = 'binary', norm=LogNorm())
+    #cmap = 'binary' cores a preto e branco
+    im1 = ax1.matshow(dPlot['P'], norm=LogNorm())
+    im2 = ax2.matshow(dPlot['N'], norm=LogNorm())
+    im3 = ax3.matshow(dPlot['B'], norm=LogNorm())
+    im4 = ax4.matshow(dPlot['R'], norm=LogNorm())
+    im5 = ax5.matshow(dPlot['Q'], norm=LogNorm())
+    im6 = ax6.matshow(dPlot['K'], norm=LogNorm())
 else:
     im1 = ax1.matshow(dPlot['P'])
     im2 = ax2.matshow(dPlot['N'])
