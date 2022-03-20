ef024540bf84e532b5831dbf3d69ebf3d26aa80b
jotaalvim <joaoafonsoalvim@gmail.com>
2022-03-20 12:11:12 +0000

    functions

--- densidade.py
+++ densidade.py
@@ -7,7 +7,6 @@ import re
 from jjcli import * 
 #import sys
 
-#lances de cavalo
 #grep -Po '\bNx?\K[a-h][1-8]' /home/jotaalvim/Documents/bases_dados/outras/lichess_jotaalvim_2021-03-23.pgn | sort | uniq -c  | sort -n
 
 #jogos = sys.argv[1]
@@ -17,7 +16,7 @@ jogos = "/home/jotaalvim/Documents/bases_dados/outras/lichess_jotaalvim_2022-03-
 #jogos = "/home/jotaalvim/Downloads/lichess_Lucena0202_2022-03-11.pgn"
 #jogos = "/home/jotaalvim/Downloads/lichess_Portomas_2022-03-11.pgn"
 #jogos = "/home/jotaalvim/Downloads/lichess_LordVeldergrath_2022-03-13.pgn" 
-#jogos = "/home/jotaalvim/Downloads/lichess_Tigran-Harutyunyan_2022-03-15.pgn" 
+jogos2 = "/home/jotaalvim/Downloads/lichess_Tigran-Harutyunyan_2022-03-15.pgn" 
 
 
 #c = clfilter(opt="do:")     ## option values in c.opt dictionary
@@ -31,123 +30,121 @@ texto = f.read()
 username = "jotaalvim"
 #username = "Portomas"
 #username = "LordVeldergrath"
-#username = "Tigran-Harutyunyan"
-
-
-#color of analysis  'black', 'white', 'both'
-#color = 'white'
-#color = 'black'
-
+username2 = "Tigran-Harutyunyan"
 
 c = clfilter(opt="blu:e:")
 
 
+#Number of moves option
 if '-e' in c.opt:
     end = int(c.opt['-e'])
 else:
     end = 300
-
-
+#Username option
 if '-u' in c.opt:
     username = c.opt['-u']
-
-
 #Logarithm scale option
 if '-l' in c.opt:
     log = True
 else:
     log = False
-
+#Black color option
 if '-b' in c.opt:
     color = 'black'
 else:
     color = 'white'
 
-
-def getGames(color):
+def getGames(color,path, username):
+    f = open(path, "r") 
+    text = f.read() 
     if (color == 'white'):
-        games = re.findall(fr'White "{username}"(?:.|\n)*?\n(1\..*)',texto)
+        games = re.findall(fr'White "{username}"(?:.|\n)*?\n(1\..*)',text)
     if (color == 'black'):
-        games = re.findall(fr'Black "{username}"(?:.|\n)*?\n(1\..*)',texto)
+        games = re.findall(fr'Black "{username}"(?:.|\n)*?\n(1\..*)',text)
     return games
 
-games = getGames(color)
 
-dic = { 'N':{}, 'B':{}, 'Q':{}, 'K':{}, 'R':{}, 'P':{} } 
+def moveTable (color, path, username):
+    games = getGames(color, path, username)
 
-for jogo in games:
-    i = 1
+    dic = { 'N':{}, 'B':{}, 'Q':{}, 'K':{}, 'R':{}, 'P':{} } 
 
-    gameSlice = re.findall(fr'\b{i}\. (.*?)(?:{end+1}|1000)',jogo+'1000')
-    if gameSlice == []:
-        gameSlice = ''
-    else:
-        gameSlice  = gameSlice[0]
+    for jogo in games:
+        i = 1
 
-    if (color == 'black'):
-        pretas = r'\S+ '
-    else:
-        pretas = ''
-
-    for peca,pos in re.findall(fr'\b[0-9]+\. {pretas}([NQBRK])x?([a-z][1-8])', gameSlice):
-        if pos in dic[peca]:
-            dic[peca][pos] += 1
+        gameSlice = re.findall(fr'\b{i}\. (.*?)(?:{end+1}|1000)',jogo+'1000')
+        if gameSlice == []:
+            gameSlice = ''
         else:
-            dic[peca][pos]  = 1
+            gameSlice  = gameSlice[0]
 
-    for pos in re.findall(fr'[0-9]+\. {pretas}(?:[a-h]x)?([a-z][1-8])',gameSlice):
-        if pos in dic['P']:
-            dic['P'][pos] += 1
+        if (color == 'black'):
+            pretas = r'\S+ '
         else:
-            dic['P'][pos]  = 1
+            pretas = ''
+
+        for peca,pos in re.findall(fr'\b[0-9]+\. {pretas}([NQBRK])x?([a-z][1-8])', gameSlice):
+            if pos in dic[peca]:
+                dic[peca][pos] += 1
+            else:
+                dic[peca][pos]  = 1
+
+        for pos in re.findall(fr'[0-9]+\. {pretas}(?:[a-h]x)?([a-z][1-8])',gameSlice):
+            if pos in dic['P']:
+                dic['P'][pos] += 1
+            else:
+                dic['P'][pos]  = 1
 
 
 # quando não ha valores poe-nos a 0
-for c in ['a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'e1', 'e2', 'e3', 'e4', 'e5', 'e6', 'e7', 'e8', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8']:
-    for key in 'NQBPKR':
-        if c not in dic[key]:
-            dic[key][c] = 0
-
+    for c in ['a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'e1', 'e2', 'e3', 'e4', 'e5', 'e6', 'e7', 'e8', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8']:
+        for key in 'NQBPKR':
+            if c not in dic[key]:
+                dic[key][c] = 0
 
 # cria uma lista com os valores
-l = { 'N' : [], 'B' : [], 'Q' : [], 'R' : [], 'P' : [], 'K' : [] }
+    l = { 'N' : [], 'B' : [], 'Q' : [], 'R' : [], 'P' : [], 'K' : [] }
 
-for key in 'NBQRPK':
-    for val in sorted(dic[key]):
-        l[key].append(dic[key][val])
+    for key in 'NBQRPK':
+        for val in sorted(dic[key]):
+            l[key].append(dic[key][val])
 
 #for val in sorted(dKing):
 #    lK.append(dKing[val])
 
 
 # parte uma lista numa matriz de listas de tamanho 8
-n = 8
+    n = 8
 
-dPlot = { 'N':None, 'Q':None, 'P':None, 'K':None, 'R':None, 'B':None}
+    dPlot = { 'N':None, 'Q':None, 'P':None, 'K':None, 'R':None, 'B':None}
 #    "plot Night"
-for key in 'NQPKRB':
-    dPlot[key] = [l[key][i:i + n] for i in range(0, len(l[key]), n)] 
+    for key in 'NQPKRB':
+        dPlot[key] = [l[key][i:i + n] for i in range(0, len(l[key]), n)] 
 
 #pK = [lK[i:i + n] for i in range(0, len(lK), n)] 
 
 # trapalhada para por o tabuleiro com orientação das brancas
 
-for key in 'NQPKRB':
-    for row in dPlot[key]:
-        row.reverse()
+    for key in 'NQPKRB':
+        for row in dPlot[key]:
+            row.reverse()
 #for row in pK:
 #    row.reverse()
 
-for key in 'NQPKRB':
-    numpy_array = np.array(dPlot[key])
-    transpose = numpy_array.T
-    dPlot[key] = transpose.tolist()
+    for key in 'NQPKRB':
+        numpy_array = np.array(dPlot[key])
+        transpose = numpy_array.T
+        dPlot[key] = transpose.tolist()
+
+    return dPlot
 
 #numpy_array = np.array(pK)
 #transpose = numpy_array.T
 #pK = transpose.tolist()
 
 #plt.title( "Knight heat map" )
+dPlot  = moveTable(color, jogos , username) 
+dPlot2 = moveTable(color, jogos2, username2) 
 
 fig, axes = plt.subplots(ncols=6, figsize=(80, 15))
 #fig.set_figheight(80)
