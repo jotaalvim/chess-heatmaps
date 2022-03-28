52e9b72f8b56063e8976e8665db1f27aa4690f2e
jotaalvim <joaoafonsoalvim@gmail.com>
2022-03-28 14:30:01 +0100

    +

--- densidade.py
+++ densidade.py
@@ -14,9 +14,10 @@ import math
 jogos2 = "/home/jotaalvim/Documents/bases_dados/outras/lichess_jotaalvim_2022-03-12.pgn"
 jogos = "/home/jotaalvim/Downloads/lichess_DiogoCipreste_2022-03-12.pgn"
 #jogos = "/home/jotaalvim/Downloads/lichess_Lucena0202_2022-03-11.pgn"
-jogos4 = "/home/jotaalvim/Downloads/lichess_Portomas_2022-03-11.pgn"
 #jogos = "/home/jotaalvim/Downloads/lichess_LordVeldergrath_2022-03-13.pgn" 
 jogos3 = "/home/jotaalvim/Downloads/lichess_Tigran-Harutyunyan_2022-03-15.pgn" 
+jogos4 = "/home/jotaalvim/Downloads/lichess_Portomas_2022-03-11.pgn"
+jogos5 = "/home/jotaalvim/Downloads/lichess_rp_o_02_2022-03-28.pgn"
 
 
 #c = clfilter(opt="do:")     ## option values in c.opt dictionary
@@ -29,12 +30,17 @@ texto = f.read()
 #username of the person in study, might be automated later?
 username = "DiogoCipreste"
 username2= "jotaalvim"
-username4 = "Portomas"
-#username = "LordVeldergrath"
 username3 = "Tigran-Harutyunyan"
+username4 = "Portomas"
+username5 = "rp_o_02"
 
-c = clfilter(opt="blu:f:t:")
 
+c = clfilter(opt="blu:f:t:p:")
+
+#FIXME
+#fazer opçao para meter paths como argumento
+#if '-p' in c.opt:
+#    jogo = c.opt['-p']
 
 #Number of moves option, t stands for 'to' 
 if '-t' in c.opt:
@@ -74,7 +80,6 @@ def getGames(color,path, username):
 
 def moveTable (color, path, username):
     games = getGames(color, path, username)
-
     dic = { 'N':{}, 'B':{}, 'Q':{}, 'K':{}, 'R':{}, 'P':{} } 
 
     for jogo in games:
@@ -155,10 +160,7 @@ def moveTable (color, path, username):
                 dic['R']['d8'] = n
 
     # quando não ha valores poe-nos a 0
-    for c in ['a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'e1', 'e2', 'e3', 'e4', 'e5', 'e6', 'e7', 'e8', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8']:
-        for key in 'NQBPKR':
-            if c not in dic[key]:
-                dic[key][c] = 0
+    fill0(dic)
 
     # cria uma lista com os valores
     l = { 'N' : [], 'B' : [], 'Q' : [], 'R' : [], 'P' : [], 'K' : [] }
@@ -191,7 +193,12 @@ def moveTable (color, path, username):
 
 
 
-#plt.title( "Knight heat map" )
+def fill0(dic):
+    for pos in ['a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'e1', 'e2', 'e3', 'e4', 'e5', 'e6', 'e7', 'e8', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8']:
+        for key in 'NQBPKR':
+            if pos not in dic[key]:
+                dic[key][pos] = 0
+
 
 def countGames (color, path,username):
     return len(getGames(color,path, username))
@@ -216,17 +223,69 @@ def difTable (color, jogos, jogos2 ,username, username2):
 
 
 
+def capturePieces (color,path, username):
+    games = getGames(color, path, username)
+    dic = { 'N' : {}, 'B' : {}, 'Q' : {}, 'R' : {}, 'P' : {}, 'K' : {} }
+    if (color == 'black'):
+        pretas = r'\S+ '
+    else:
+        pretas = ''
+
+    for jogo in games:
+        gameSlice = re.findall(fr'\b{i}\. (.*?)(?:{end+1}|1000)',jogo+'1000')
+        if gameSlice == []:
+            gameSlice = ''
+        else:
+            gameSlice  = gameSlice[0]
+
+        for piece,pos in re.findall(fr'\b[0-9]+\. {pretas}([NQBRK])x([a-z][1-8])', gameSlice):
+            if pos in dic[piece]:
+                dic[piece][pos] += 1
+            else:
+                dic[piece][pos] =  1
+
+        for pos in re.findall(fr'[0-9]+\. {pretas}[a-h]x([a-h][1-8])\+?',gameSlice):
+            if pos in dic['P']:
+                dic['P'][pos] += 1
+            else:
+                dic['P'][pos]  = 1
+
+
+    fill0(dic)
+    # cria uma lista com os valores
+    l = { 'N' : [], 'B' : [], 'Q' : [], 'R' : [], 'P' : [], 'K' : [] }
+
+    for key in 'NBQRPK':
+        for val in sorted(dic[key]):
+            l[key].append(dic[key][val])
+
+    # parte uma lista numa matriz de listas de tamanho 8
+    n = 8
+    dPlot = { 'N':None, 'Q':None, 'P':None, 'K':None, 'R':None, 'B':None}
+    for key in 'NQPKRB':
+        dPlot[key] = [l[key][i:i + n] for i in range(0, len(l[key]), n)]
+    for key in 'NQPKRB':
+        for row in dPlot[key]:
+            row.reverse()
+    for key in 'NQPKRB':
+        numpy_array = np.array(dPlot[key])
+        transpose = numpy_array.T
+        dPlot[key] = transpose.tolist()
+
+    return dPlot
+
+
 #dPlot = difTable (color, jogos2, jogos3, username2 , username3)
 
 #dPlot  = moveTable(color, jogos , username) 
 #dPlot  = moveTable(color, jogos2 , username2) 
-dPlot  = moveTable(color, jogos3 , username3) 
+#dPlot  = moveTable(color, jogos3 , username3) 
 #dPlot  = moveTable(color, jogos4 , username4) 
+#dPlot  = moveTable(color, jogos5 , username5) 
+dPlot  = capturePieces(color, jogos3 , username3) 
 
 fig, axes = plt.subplots(ncols=6, figsize=(80, 15))
-
 ax1, ax2, ax3, ax4, ax5, ax6 = axes
-
 if (log):
     #cmap = 'binary' cores a preto e branco
     im1 = ax1.matshow(dPlot['P'], norm=LogNorm())
